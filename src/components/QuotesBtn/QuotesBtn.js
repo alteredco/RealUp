c9d9005ad7fe57f3c037579e03e6539cdb7b45ac
Louise Hayes <louise@deewhy.ie>
2018-10-10 23:38:49 -0400

    first commit-scaffolding for components, pages and homepage

---
+++ src/components/QuotesBtn/QuotesBtn.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./QuotesBtn.css";
+
+// The ...props means, spread all of the passed props onto this element
+// That way we don't have to define them all individually
+const QuotesBtn = props => (
+<button className="btn-float" onClick={props.onClick} >
+            {props.label}
+</button> 
+
+
+//   <span className="quotes-btn btn-float" {...props}>
+// +
+//   </span>
+);
+
+export default QuotesBtn;
