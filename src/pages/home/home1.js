be4fc33283a55219512544b51fa2c1cbb99aea96
Wendy <alteredco@gmail.com>
2018-10-15 13:47:52 +0200

    add starting image component

--- src/pages/home/home1.js
+++ src/pages/home/home1.js
@@ -1,8 +1,16 @@
 import React, {Component} from "react";
-
+import logo from '../../../public/assets/images/momscancodepghlogo.jpg';
 class Home1 extends Component {
-  render() {return <div>This is Home 1</div>}
+  render() {
+    return (
+    <div className="row">
+      <div className="logoImg">
+      <img alt="logo placeholder" src={logo} />
+      </div>
+    </div>
+    )
+  }
 }
-      
+
 
 export default Home1;
