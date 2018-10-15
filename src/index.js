cad853218344fda0813afe5372dddbfcd5b3197c
Wendy <alteredco@gmail.com>
2018-10-15 12:33:40 +0200

    add switch to react-router-dom imports to fix root path route bug

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home1 from './pages/home/home1.js';
 
 
 ReactDOM.render(
   <BrowserRouter>
     <div>
-      Header Placeholder...
-      <Route path="/" component={Home1} />
+      <Switch>
+       {/*  /* IMPORTANT! add any other routes ABOVE the home1 Route !*/ */}
+        <Route path="/" component={Home1} />
+      </Switch>
     </div>
   </BrowserRouter>,
   document.getElementById('root')
