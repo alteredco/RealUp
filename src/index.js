cf34e25d4825065be3722b3dbe2b406f42e31af1
Wendy <alteredco@gmail.com>
2018-10-15 12:17:20 +0200

    add react-router-dom setup for index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from "./App";
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home1 from './pages/home/home1.js';
+
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <div>
+      Header Placeholder...
+      <Route path="/" component={Home1} />
+    </div>
+  </BrowserRouter>,
   document.getElementById('root')
 )
