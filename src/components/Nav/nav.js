c4cc70a24aecfda67923bd941f75962f1dd08ca3
Louise Hayes <louise@deewhy.ie>
2018-10-15 15:43:40 -0400

    adding back button to header and next button

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "../Button";
 
 class Nav extends React.Component {
 
@@ -7,6 +8,9 @@ class Nav extends React.Component {
         return(
 
         <div className="center-align"> 
+        <Link to="/">
+                    <Button label="Back"/>
+        </Link>	
 			<Link to="/">
                     <img height="200px" width="200px" alt="logo placeholder" src="./assets/images/REALUPLOGO1-1.png" />
             </Link>	
