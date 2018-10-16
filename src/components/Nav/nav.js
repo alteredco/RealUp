2139d7ed04b98cee7a6082d5897f1fcf76d2420e
Louise Hayes <louise@deewhy.ie>
2018-10-16 12:38:35 -0400

    back button working

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -1,22 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "../Button";
+// import Button from "../Button";
+// import { browserHistory } from 'react-router';
+
+
+
 
 class Nav extends React.Component {
+    constructor(props){
+        super(props);
+        this.goBack=this.goBack.bind(this);
+    }
 
+    goBack(){
+        window.history.back();
+    }
+    
     render(){
         return(
 
         <div className="center-align"> 
-        <Link to="/">
-                    <Button label="Back"/>
-        </Link>	
-			<Link to="/">
+            <button onClick={this.goBack}>Go Back</button>
+
+                    {/* <Button className="center-align" label="Back"/> */}
+
+
+		<Link to="/">
                     <img height="200px" width="200px" alt="logo placeholder" src="./assets/images/REALUPLOGO1-1.png" />
-            </Link>	
+        </Link>	
     
         </div>
     )}
+    
 }
 
 export default Nav;
