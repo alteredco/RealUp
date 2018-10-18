af561acfd1d87942d46d242c1d8e1c6caa2174bf
Sara Torrey <sara.moravej@gmail.com>
2018-10-18 14:56:26 -0500

    thougths 3 text alignment

--- src/components/InputListItem/InputListItem.js
+++ src/components/InputListItem/InputListItem.js
@@ -5,10 +5,10 @@ import "./InputListItem.css";
 // That way we don't have to define them all individually
 const InputListItem = props => (
     <div className="container">
-            <div className="input-field col s12 m12 l12 offset-m2 offset-l2 flow-text">
+            <div className="input-field col s12 m12 l12 flow-text">
                 <label className="active">{props.label}</label>  
                 <input
-                    className="materialize-textarea"
+                    
                     type="text"
                     placeholder={props.placeholder}
                 />
