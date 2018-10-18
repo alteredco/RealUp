88d2bec0a58c6e1ee65b2b32d03eb0bf7fa2485b
MarikaKononen <marika.kononen@koodielamastani.fi>
2018-10-18 09:24:35 +0300

    Add materialize offset values to timer and some pages

--- src/components/InputListItem/InputListItem.js
+++ src/components/InputListItem/InputListItem.js
@@ -4,7 +4,7 @@ import "./InputListItem.css";
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
 const InputListItem = props => (
-    <div className="container">
+  
             <div className="input-field col s12 m8 l8 offset-m2 offset-l2 flow-text">
                 <label className="active">{props.label}</label>  
                 <input
@@ -12,7 +12,7 @@ const InputListItem = props => (
                     placeholder={props.placeholder}
                 />
         </div>
-    </div>
+
     
 );
 
