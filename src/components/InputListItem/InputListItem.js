88f03d4039f22eddc8b921580f80b7a8c77d1fdc
MarikaKononen <marika.kononen@koodielamastani.fi>
2018-10-18 22:01:16 +0300

    removed s+m+l+xl and changed inputlistitem m & l values

--- src/components/InputListItem/InputListItem.js
+++ src/components/InputListItem/InputListItem.js
@@ -5,7 +5,7 @@ import "./InputListItem.css";
 // That way we don't have to define them all individually
 const InputListItem = props => (
     <div className="container">
-            <div className="input-field col s12 m12 l12 offset-m2 offset-l2 flow-text">
+            <div className="input-field col s12 m8 l8 offset-m2 offset-l2 flow-text">
                 <label className="active">{props.label}</label>  
                 <input
                     className="materialize-textarea"
