b9c7356a40700d7e65bf799a584684a7a108a011
MarikaKononen <marika.kononen@koodielamastani.fi>
2018-10-19 00:50:19 +0300

    corrected cypress tests of home page

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -1,24 +1,24 @@
-describe('RealUp Test App: Home', function(){
+describe('RealUp Test App:Home2', function(){
     it('Test cypress Works!', function(){
         expect(true).to.equal(true)
     })
-    it('Visits RealUp Home Page', function(){
+    it('Visits RealUp Home Page 2', function(){
         cy.visit('/')
     })
-    it("Clicks The Let's Get Stated button", function(){
+    it("Clicks The Let's Get Started button", function(){
         cy.visit('/')
         cy.contains("Let's Get Started").click()
-
     })
-    it("The Let's Get Stated button leads to home2", function(){
+    it("Clicks The Back button", function(){
+        cy.visit('/')
+        cy.get('#backbutton').click()
+    })
+    it("The Let's Get Started button leads to home3", function(){
         cy.visit('/')
         cy.contains("Let's Get Started").click()
-        cy.url().should('eq', 'http://localhost:3000/home2')
+        cy.url().should('eq', 'http://localhost:3000/home3')
         
     })
-
-    
-    
 })
 
 
