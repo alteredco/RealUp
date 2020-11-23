f9592cafa90bb95fc1463a3e6f2a51f8d6d59c96
Louise Hayes <louise.hayes@workgrid.com>
2020-11-23 08:10:31 -0500

    test message

--- src/pages/feelings/feelings1.js
+++ src/pages/feelings/feelings1.js
@@ -1,27 +1,36 @@
-import React, {Component} from "react";
-import TextLines from '../../components/TextLines/TextLines';
-import Button from '../../components/Button/Button';
-import {Link } from 'react-router-dom';
-
+import React, { Component } from "react";
+import TextLines from "../../components/TextLines/TextLines";
+import Button from "../../components/Button/Button";
+import { Link } from "react-router-dom";
+//testing a commit
 class Feelings1 extends Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-          feelings1Txt: "Impostor Syndrome can cause feelings of anxiety, fear, and depression.",
-          feelings2Txt: "Real Up is here to help you manage through this physiological response.",
-          feelings1Btn: "I'm Ready",  
-
-        }
-      }
-    render() {
-      return (
-        <div className="row center-align">
-            <div ><TextLines text={this.state.feelings1Txt} /></div>
-            <div><TextLines text={this.state.feelings2Txt}/></div>
-            <div className="row"><Link to="/feelings2"><Button label={this.state.feelings1Btn}/></Link></div>
-        </div>      
-      )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      feelings1Txt:
+        "Impostor Syndrome can cause feelings of anxiety, fear, and depression.",
+      feelings2Txt:
+        "Real Up is here to help you manage through this physiological response.",
+      feelings1Btn: "I'm Ready",
+    };
   }
+  render() {
+    return (
+      <div className="row center-align">
+        <div>
+          <TextLines text={this.state.feelings1Txt} />
+        </div>
+        <div>
+          <TextLines text={this.state.feelings2Txt} />
+        </div>
+        <div className="row">
+          <Link to="/feelings2">
+            <Button label={this.state.feelings1Btn} />
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Feelings1;
